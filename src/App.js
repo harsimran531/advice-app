298960089267f24c8998c9479aee4f41affe5689
simran <simran@eventus-software.in>
2020-05-29 12:10:52 +0530

    improved design

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { Component } from "react";
+import BackgroundImageOnLoad from "background-image-on-load";
 import './App.css';
 import axios from 'axios';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-class App extends React.Component {
-  state = { advice: '' }
-
+class App extends Component {
+  state = {
+    bgIsLoaded: false,
+    advice: '',
+  };
   componentDidMount() {
-    this.fetchAdvice();
-  }
-  fetchAdvice = () => {
+     this.fetchAdvice();
+    }
+    fetchAdvice = () => {
     axios.get('https://api.adviceslip.com/advice')
       .then((response) => {
         const { advice } = response.data.slip;
@@ -19,22 +28,62 @@ class App extends React.Component {
         console.log(error);
       })
   }
+  loadPage(){
+    console.log('clicked')
+    window.location.reload();
+  }
   render() {
-    const { advice } = this.state;
+    const { bgIsLoaded,advice } = this.state;
     return (
-      <div className="app">
+      
+      <div
+        style={{
+          height: "100vh",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundImage: `url(${
+            !bgIsLoaded
+              ? "https://unsplash.it/1200?nature"
+              : "https://unsplash.it/1200?city"
+          })`
+        }}>
+        <BackgroundImageOnLoad
+          src={"https://unsplash.it/1200?random"}
+          onLoadBg={() =>
+            this.setState({
+              bgIsLoaded: true
+            })
+          }
+          onError={err => console.log("error", err)}
+        />
+        <div className="app">
         <div className="card">
-            <h1 className="heading">{advice}</h1>
-            <button className="button" onClick={this.fetchAdvice}>
-              <span>
-                GET AN ADVICE!
-              </span>
-            </button>
-        </div>
+          <Card>
+           <CardActionArea>
+             <CardContent>
+               <Typography variant="h5" component="h5" className="heading">
+                 {advice}
+               </Typography>
+             </CardContent>
+           </CardActionArea>
+           <CardActions>
+             <Button size="medium" color="primary" className="button" variant="outlined" onClick={() => {
+          this.fetchAdvice();
+          this.loadPage();
+          }}>
+               GET AN ADVICE!
+         </Button>
+           </CardActions>
+         </Card>
       </div>
-    )
+      </div>
+      </div>
+    );
   }
 }
 
 
 export default App;
+
+
+
